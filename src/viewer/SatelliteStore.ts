0700c7872c31b0b041e022c7ae0716faf81c2e3d
Andre-John Mas <andrejohn.mas@gmail.com>
2025-12-20 19:18:10 +0100

    use of indexdb and update github workflow

--- src/viewer/SatelliteStore.ts
+++ src/viewer/SatelliteStore.ts
@@ -3,31 +3,40 @@ import EventManager from '../utils/event-manager';
 import logger from '../utils/logger';
 import { SatelliteObject } from './interfaces/SatelliteObject';
 import { ensureJsDate } from '@/utils/dateUtils';
+import { DateTime } from 'luxon';
 
-type TLE = unknown;
+type TLE = Partial<SatelliteObject>;
 
-type attributedTLE = {
+type AttributedTLE = {
   source: {
     name: string;
     url: string;
   }
   date: string | Date;
-  data: TLE;
+  data: TLE[];
+};
+
+type CacheData = {
+  data: AttributedTLE,
+  storageTime: Date
 };
 
 type SatelliteStoreConfig = {
-  cacheLocal: boolean;
+  useCache: boolean;
   cacheTTL: number;
   baseUrl: string;
 }
 
 const config: SatelliteStoreConfig = {
-  cacheLocal: true,
+  useCache: true,
   cacheTTL: 24 * 60 * 60,
   baseUrl: import.meta.env.BASE_URL
 };
 
 class SatelliteStore {
+  databaseName = 'stuff-in-space';
+  storageKey = 'satellite.data';
+  databaseVersion = 2;
   tleUrl = `${config.baseUrl}/data/-TLE.json`;
   eventManager: EventManager;
   satData: SatelliteObject[] = [];
@@ -50,36 +59,201 @@ class SatelliteStore {
     }
   }
 
-  loadFromCache () {
-    localStorage.getItem('satellite.data');
+  async setupDatabase () {
+    if (!window.indexedDB) {
+      return;
+    }
+
+    const storeNames = ['configs', 'tles'];
+
+    const dbRequest = window.indexedDB.open(this.databaseName, this.databaseVersion);
+
+    return new Promise<IDBDatabase | undefined>((resolve, reject) => {
+      dbRequest.addEventListener('upgradeneeded', () => {
+        const database = dbRequest.result;
+        storeNames.forEach(storeName => {
+          if (!database.objectStoreNames.contains(storeName)) {
+            database.createObjectStore(storeName);
+          }
+        });
+      });
+      dbRequest.addEventListener('success', () => {
+        const database = dbRequest.result;
+        resolve(database);
+      });
+
+      dbRequest.addEventListener('error', (error) => {
+        reject(error);
+      });
+    });
+  }
+
+  async getObjectStore (storeName: string) {
+    if (!window.indexedDB) {
+      return;
+    }
+
+    const database = await this.setupDatabase();
+
+    if (database) {
+      try {
+        const transaction = database.transaction(storeName, 'readwrite');
+        return transaction.objectStore(storeName);
+      } catch (error) {
+        console.error(`Error while fetching ${storeName}`, error);
+      }
+    }
+  }
+
+  async loadFromStorage () {
+    if (!window.indexedDB) {
+      return;
+    }
+
+    let attributedTle: AttributedTLE | undefined;
+    let tleList: TLE[] | undefined;
+    let tleAge: Record<string, unknown> | undefined;
+
+    const configStore = await this.getObjectStore('configs');
+    const dbRequest = configStore?.get('attributedTle');
+    if (dbRequest) {
+      attributedTle = await new Promise<AttributedTLE | undefined>((resolve, reject) => {
+        dbRequest.onerror = reject;
+        dbRequest.onsuccess = () => {
+          resolve(dbRequest.result);
+        };
+      });
+    }
+
+    const dbRequest2 = configStore?.get('tleAge');
+    if (dbRequest2) {
+      tleAge = await new Promise<Record<string, unknown> | undefined>((resolve, reject) => {
+        dbRequest2.onerror = reject;
+        dbRequest2.onsuccess = () => {
+          resolve(dbRequest2.result);
+        };
+      });
+    }
+
+    const tleStore = await this.getObjectStore('tles');
+    const tleDBRequest = tleStore?.getAll();
+    if (tleDBRequest) {
+      tleList = await new Promise<Record<string, unknown>[] | undefined>((resolve, reject) => {
+        tleDBRequest.onerror = reject;
+        tleDBRequest.onsuccess = () => {
+          resolve(tleDBRequest.result);
+        };
+      });
+    }
+
+    if (attributedTle) {
+      return {
+        storageTime: tleAge?.loadedTime as Date,
+        data: {
+          ...attributedTle,
+          data: tleList
+        }
+      } as CacheData;
+    }
+
+    return undefined;
+  }
+
+  async saveToStorage (attributedTle: AttributedTLE) {
+    if (!window.indexedDB) {
+      return;
+    }
+
+    const configStore = await this.getObjectStore('configs');
+    if (configStore) {
+      let dbRequest = configStore.put({
+        source: attributedTle.source,
+        date: attributedTle.date,
+        data: []
+      } as AttributedTLE, 'attributedTle');
+
+      if (dbRequest) {
+        await new Promise((resolve, reject) => {
+          dbRequest.onerror = reject;
+          dbRequest.onsuccess = () => {
+            resolve(dbRequest.result);
+          };
+        });
+      }
+
+      dbRequest = configStore.put({
+        loadedTime: new Date()
+      } , 'tleAge');
+
+      if (dbRequest) {
+        await new Promise((resolve, reject) => {
+          dbRequest.onerror = reject;
+          dbRequest.onsuccess = () => {
+            resolve(dbRequest.result);
+          };
+        });
+      }
+    }
+
+    const tleStore = await this.getObjectStore('tles');
+    if (tleStore) {
+      const tleData = attributedTle.data as TLE[];
+      tleData.forEach(entry => {
+        tleStore?.put(entry, entry.OBJECT_ID as string);
+      });
+    }
   }
 
   async loadSatelliteData () {
     try {
 
-      const tleUrl = new URL(this.tleUrl, window.location.href);
-      let attributedData: attributedTLE | undefined;
-      let response = await axios.get(tleUrl.toString(), {
-        params: {
-          t: Date.now()
-        }
-      });
+      let cacheData: CacheData | undefined;
+      let attributedData: AttributedTLE | undefined ;
+      let cachExpired = false;
 
-      if (response.data) {
-        attributedData = response.data;
+      if (config.useCache) {
+        cacheData = (await this.loadFromStorage()) as CacheData;
       }
 
-      // support data being in separate location
-      if (typeof attributedData?.data === 'string') {
-        const url = new URL(attributedData.data, tleUrl);
-        response = await axios.get(url.toString(), {
+      if (cacheData?.data) {
+        attributedData = cacheData.data;
+      }
+
+      if (cacheData?.storageTime) {
+        const storageTime = DateTime.fromJSDate(cacheData.storageTime);
+        const expiryTime = storageTime.plus({ seconds: config.cacheTTL });
+        cachExpired = DateTime.now() > expiryTime;
+      }
+
+      if (!cacheData || cachExpired) {
+        const tleUrl = new URL(this.tleUrl, window.location.href);
+
+        let response = await axios.get(tleUrl.toString(), {
           params: {
             t: Date.now()
           }
         });
 
         if (response.data) {
-          attributedData.data = response.data;
+          attributedData = response.data;
+        }
+
+        // support data being in separate location
+        if (typeof attributedData?.data === 'string') {
+          const url = new URL(attributedData.data, tleUrl);
+          response = await axios.get(url.toString(), {
+            params: {
+              t: Date.now()
+            }
+          });
+
+          if (response.data) {
+            attributedData.data = response.data;
+          }
+        }
+
+        if (config.useCache && attributedData) {
+          await this.saveToStorage(attributedData);
         }
       }
 
